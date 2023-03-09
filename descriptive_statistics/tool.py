dd88c2844acfc01eafe1ad7591566ae1d476bd42
Danielwu <danielwu1202@gmail.comm>
2023-03-09 16:03:59 +0800

    分析函式

--- descriptive_statistics/tool.py
+++ descriptive_statistics/tool.py
@@ -1,7 +1,7 @@
 # 根據type分類
 def type_filter(data, type):
     mask = data['Type'] == type
-    data[mask].to_csv(str(type) + 'anime.csv', index=False)
+    data[mask].to_csv(str(type) + '_anime.csv', index=False, encoding = 'utf-8')
 
 # 統計genres
 def genres_counter(data):
@@ -15,6 +15,12 @@ def theme_counter(data):
     theme_counter.columns = ['themes', 'counter']
     return theme_counter
 
+# 統計type
+def type_counter(data):
+    type_counter = data.Type.str.get_dummies(sep=', ').sum().to_frame(name = 'counter').reset_index()
+    type_counter.columns = ['type', 'counter']
+    return type_counter
+
 '''
 # 分類
 def anime_filter(data, column, genre, type):
@@ -24,4 +30,10 @@ def anime_filter(data, column, genre, type):
 
 # 分類
 def anime_filter(data, column, filter, type):
-    data[data[column].str.contains(filter)].to_csv(str(type)+ '_' +str(filter) + '.csv', index=False)
+    data[data[column].str.contains(filter)].to_csv(str(type)+ '_' +str(filter) + '.csv', index=False, encoding = 'utf-8')
+
+
+# 根據年份分類
+def year_spliter(data, start, end, step, column='Start_Aired'):
+    for year in range(start, end, step):
+        data[(data[column] >= str(year)) & (data[column] < str(year + 10))].to_csv(str(year) + '_' + str(year + 10) + '.csv', index=False, encoding='utf-8')
